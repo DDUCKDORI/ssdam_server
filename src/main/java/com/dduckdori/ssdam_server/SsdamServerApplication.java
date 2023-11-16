f7eef0c38f6456a65f3c3f9c53828e2b18efbd1a
LeeHoonDong <ldhoon0813@gmail.com>
2023-11-17 01:06:58 +0900

    first commit

---
+++ src/main/java/com/dduckdori/ssdam_server/SsdamServerApplication.java
@@ -0,0 +1,13 @@
+package com.dduckdori.ssdam_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsdamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dduckdori/ssdam_server/SsdamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dduckdori.ssdam_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
