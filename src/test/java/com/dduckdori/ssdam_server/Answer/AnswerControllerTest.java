23ce72e2b78b5e0718740006624a7e49c1485efc
LeeHoonDong <ldhoon0813@gmail.com>
2023-12-02 21:17:49 +0900

    TEST. 답변 저장 실패 테스트 진행

---
+++ src/test/java/com/dduckdori/ssdam_server/Answer/AnswerControllerTest.java
@@ -0,0 +1,57 @@
+package com.dduckdori.ssdam_server.Answer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class AnswerControllerTest {
+    MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext wac;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @BeforeEach
+    public void setup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+    @Test
+    @DisplayName("답변_저장_실패")
+    @WithMockUser
+    public void Save_Answer_Fail() throws Exception{
+        AnswerDTO answerDTO = new AnswerDTO();
+        answerDTO.setCate_id(1);
+        answerDTO.setQust_id(1);
+        answerDTO.setMem_id(2);
+        answerDTO.setInvite_cd("AAAAA00000");
+
+        this.mockMvc
+                .perform(post("/ssdam/answer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(answerDTO))
+                )
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+}
